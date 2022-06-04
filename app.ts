9ae31dcb13e7933694e1b8db1def8bd0ede5c3ce
yhrage524 <82950970+yhrage524@users.noreply.github.com>
2022-06-04 22:22:51 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -29,8 +29,8 @@ app.get('*', ( _req, res ) => {
 async function start(): Promise<void> {
     await mongoose.connect(mongoUri)
     
-    app.get('/', function(request, response) {
-        var result = 'App is running'
+    app.get('/', () => {
+        const result = 'App is running'
         response.send(result);
     }).listen(app.get('port'), function() {
             console.log('App is running, server is listening on port ', app.get('port'));
